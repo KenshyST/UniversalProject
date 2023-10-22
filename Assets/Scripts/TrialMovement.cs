cdbb7ec8a33eddb84527d5792eaab51290c8136f
Jose Lopez <jdloci05@gmail.com>
2023-10-22 00:11:08 -0500

    PickUp

--- Assets/Scripts/TrialMovement.cs
+++ Assets/Scripts/TrialMovement.cs
@@ -8,6 +8,7 @@ public class TrialMovement : MonoBehaviour
     [Header("General")]
     public float gravity = -9.81f;
     private Vector3 velocity;
+    public bool isGrabbed = false;
 
     [Header("References")]
     public Camera playerCamera;
@@ -37,7 +38,17 @@ public class TrialMovement : MonoBehaviour
     public float coyoteTime = 0.2f;
     public float internalCounterCoyoteTime;
 
+    [Header("CineMachine")]
     private CinemachineFreeLook freeLookCam;
+    private float lastTargetAngle;
+
+    // Valores originales para restaurar la cámara cuando se suelta el objeto
+    private float originalTopRigHeight;
+    private float originalMiddleRigRadius;
+
+    // Valores deseados para la transición
+    public float grabbedTopRigHeight = 7.1f;
+    public float grabbedMiddleRigRadius = 6.26f;
 
     private void Awake()
     {
@@ -53,6 +64,14 @@ public class TrialMovement : MonoBehaviour
 
     private void HandleMovement()
     {
+
+        if (isGrabbed)
+        {
+            OrientPlayerToCamera();
+            MoveWithCameraDirection(); 
+            return;
+        }
+
         if (characterController.isGrounded)
         {
             canDoubleJump = true;
@@ -73,11 +92,15 @@ public class TrialMovement : MonoBehaviour
 
         if (direction.magnitude >= 0.1f)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + playerCamera.transform.eulerAngles.y;
-            transform.rotation = Quaternion.Euler(0, targetAngle, 0);
+            lastTargetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + playerCamera.transform.eulerAngles.y;
+            transform.rotation = Quaternion.Euler(0, lastTargetAngle, 0);
 
-            Vector3 moveDir = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
+            Vector3 moveDir = Quaternion.Euler(0, lastTargetAngle, 0) * Vector3.forward;
             characterController.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(0, lastTargetAngle, 0);
         }
 
         if (Input.GetButtonDown("Jump") && (characterController.isGrounded || (internalCounterCoyoteTime > 0 && readyToJump)))
@@ -132,4 +155,41 @@ public class TrialMovement : MonoBehaviour
             gravity = -9.81f; // Restablece la gravedad a su valor predeterminado
         }
     }
+
+    public void OrientPlayerToCamera()
+    {
+        Vector3 cameraDirection = new Vector3(playerCamera.transform.forward.x, 0, playerCamera.transform.forward.z).normalized;
+        float targetAngle = Mathf.Atan2(cameraDirection.x, cameraDirection.z) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, targetAngle, 0);
+    }
+
+    private void MoveWithCameraDirection()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        // Obtenemos la dirección de movimiento relativa a la cámara.
+        Vector3 direction = playerCamera.transform.forward * vertical + playerCamera.transform.right * horizontal;
+        direction.y = 0; // Mantenemos la componente y en 0 para no moverse hacia arriba o abajo.
+        direction.Normalize(); // Normalizamos la dirección para que tenga una magnitud de 1.
+
+        float speed = Input.GetButton("Sprint") ? runSpeed : walkSpeed;
+
+        // Movemos al jugador en la dirección calculada.
+        characterController.Move(direction * speed * Time.deltaTime);
+    }
+
+
+    public void GrabObject()
+    {
+        isGrabbed = true;
+        velocity.y = 0; // Restablecer la velocidad vertical
+    }
+
+    public void ReleaseObject()
+    {
+        isGrabbed = false;
+    }
+
+
 }
